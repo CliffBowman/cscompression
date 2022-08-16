75320388d7d202f901bcffaa337647f118718387
cliffbowman <github@cliffbowman.com>
2022-08-16 15:28:33 -0700

    Updated test to use to RLE algorithm

--- tests/FullCompressionTests.cs
+++ tests/FullCompressionTests.cs
@@ -13,34 +13,42 @@ public class FullCompressionTests
         _output = output;
     }
 
-    private void outputStats(int inputLength, int outputLength)
+    private void outputStats(string label, int inputLength, int outputLength)
     {
-        _output.WriteLine($"=== {inputLength} in, {outputLength} out, {((double)outputLength / (double)inputLength) * 100}% of original size. ===");
+        var percent = string.Format("{0:F2}",((double)outputLength / (double)inputLength) * 100);
+        _output.WriteLine($"=== {label} {inputLength} in, {outputLength} out, {percent}% of original size. ===");
     }
 
     [Fact]
     // Not all Bzip2 steps are present quite yet.
-    public void Bzip2Test()
+    public void Bzip2ishTest()
     {
         var bytes = (string data) => Encoding.UTF8.GetBytes(data);
         var str = (byte[] data) => Encoding.UTF8.GetString(data);
         var codebook = new Dictionary<byte, bool[]>();
-        var input = File.ReadAllText("don_quixote.txt").Substring(0, 500);
+        var input = File.ReadAllBytes("don_quixote.txt").Take(100_000).ToArray();
 
         // Encode steps
-        var outputRLE1 = new RunLengthEncoding().EncodeBytes(bytes(input));
-        var outputBWT = new BurrowsWheelerTransform().Encode(outputRLE1);
-        var outputMTF = new MoveToFrontTransform().Encode(outputBWT.data);
-        var outputRLE2 = new RunLengthEncoding().EncodeBytes(outputMTF);
-        var outputBits = new HuffmanEncoding().Encode(outputRLE2, ref codebook);
+        var outputRLE1 = new RunLengthEncodingRLE90Impl().Encode(input);
+        outputStats("RLE90", input.Length, outputRLE1.Length);
 
-        outputStats(input.Length, outputBits.Length / 8);
+        var outputBWT = new BurrowsWheelerTransform().Encode(outputRLE1);
+        outputStats("BWT", outputRLE1.Length, outputBWT.data.Length);
+
+        var outputMTF = new MoveToFrontTransform().Encode(outputBWT.data);
+        outputStats("MTF", outputBWT.data.Length, outputMTF.Length);
+
+        var outputRLE2 = new RunLengthEncodingRLE90Impl().Encode(outputMTF);
+        outputStats("RLE90", outputMTF.Length, outputRLE2.Length);
+
+        var outputBits = new HuffmanEncoding().Encode(outputRLE2, ref codebook);
+        outputStats("Huffman", input.Length, outputBits.Length / 8);
 
         // Decode steps
         var decodedHuffman = new HuffmanEncoding().Decode(outputBits, codebook);
         Assert.Equal(decodedHuffman, outputRLE2);
 
-        var decodedRLE = new RunLengthEncoding().DecodeBytes(decodedHuffman);
+        var decodedRLE = new RunLengthEncodingRLE90Impl().Decode(decodedHuffman);
         Assert.Equal(decodedRLE, outputMTF);
 
         var decodedMTF = new MoveToFrontTransform().Decode(decodedRLE);
@@ -49,7 +57,7 @@ public class FullCompressionTests
         var decodedBWT = new BurrowsWheelerTransform().Decode(decodedMTF, outputBWT.index);
         Assert.Equal(decodedBWT, outputRLE1);
 
-        decodedRLE = new RunLengthEncoding().DecodeBytes(decodedBWT);
-        Assert.Equal(decodedRLE, bytes(input));
+        decodedRLE = new RunLengthEncodingRLE90Impl().Decode(decodedBWT);
+        Assert.Equal(decodedRLE, input);
     }
 }
